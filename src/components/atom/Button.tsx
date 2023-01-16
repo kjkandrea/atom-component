e80ec4403ae047286f955ae2291c83ec4497f90f
kjkandrea <kjkandrea@gmail.com>
2023-01-16 18:37:26 +0900

    Revert "feat: href 시 자동 추론 & as 컴포넌트 구현"
    This reverts commit b3a397b9

--- src/components/atom/Button.tsx
+++ src/components/atom/Button.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 
-// 1.
-type ButtonElementProps = JSX.IntrinsicElements['button'];
-type AnchorElementProps = JSX.IntrinsicElements['a'];
+const defaultElement = 'button';
 
-// 2.
-type PolymorphicElementProps<T extends React.ElementType> = {
-  as: T;
+type ButtonProps<T extends React.ElementType> = {
+  as?: T;
 } & React.ComponentPropsWithoutRef<T>;
 
-// 3.
-type ButtonProps<T extends React.ElementType> =
-  | ButtonElementProps
-  | AnchorElementProps
-  | PolymorphicElementProps<T>;
+export default function Button<
+  T extends React.ElementType = typeof defaultElement
+>({as, ...rest}: ButtonProps<T>) {
+  const Element = as ?? defaultElement;
 
-// 4.
-function isPropsForPolymorphicElement<T extends React.ElementType>(
-  props: ButtonProps<React.ElementType>
-): props is PolymorphicElementProps<T> {
-  return 'as' in props;
-}
-
-function isPropsForAnchorElement(
-  props: ButtonProps<React.ElementType>
-): props is AnchorElementProps {
-  return 'href' in props;
-}
-
-export default function Button<T extends React.ElementType>(
-  props: ButtonProps<T>
-) {
-  if (isPropsForPolymorphicElement<T>(props)) {
-    const {as, ...rest} = props;
-    const Element: React.ElementType = as;
-    return <Element {...rest} />;
-  } else if (isPropsForAnchorElement(props)) {
-    return <a {...props} />;
-  } else {
-    return <button {...props} />;
-  }
+  return <Element {...rest} />;
 }
